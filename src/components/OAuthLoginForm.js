dceac68c53774a3618bb5b91d2e7ae77a72f5975
SogCheonBul <jy6443@naver.com>
2024-12-17 23:53:17 +0900

    Add: 비밀번호 찾기 추가

--- src/components/OAuthLoginForm.js
+++ src/components/OAuthLoginForm.js
@@ -1,28 +1,26 @@
 import React from "react";
 import Button from "./common/Button";
-
-const api = "http://52.78.186.21:8080/book";
-// const api = "http://localhost:8080/book";
+import { handleOAuthLogin } from "../services/AuthService";
 
 function OAuthLoginForm() {
-  const handleOAuthLogin = (provider) => {
-    // OAuth 인증 URL로 이동
-    window.location.href = `${api}/auth/${provider}/login`;
-  };
   return (
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <div className="flex justify-center items-center space-x-4">
         <Button
-          className="oauth-button kakao"
+          variant="outlined"
+          className="oauth-button kakao flex items-center space-x-2"
           onClick={() => handleOAuthLogin("kakao")}
         >
-          카카오 로그인
+          <img src="/kakao.png" alt="카카오 아이콘" className="w-5 h-5" />
+          <span>카카오 로그인</span>
         </Button>
         <Button
-          className="oauth-button naver"
+          variant="outlined"
+          className="oauth-button naver flex items-center space-x-2"
           onClick={() => handleOAuthLogin("naver")}
         >
-          네이버 로그인
+          <img src="/naver.png" alt="네이버 아이콘" className="w-5 h-5" />
+          <span>네이버 로그인</span>
         </Button>
       </div>
     </div>
