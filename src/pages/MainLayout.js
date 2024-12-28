5a7c8a139baa919ee61fd219409d143ed6315930
SogCheonBul <jy6443@naver.com>
2024-12-28 12:26:58 +0900

    Fix: MainLayout 수정

--- src/pages/MainLayout.js
+++ src/pages/MainLayout.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Header from "../components/common/Header";
 // import Footer from "../components/common/Footer";
-import Banner from "../components/common/Banner";
+// import Banner from "../components/common/Banner";
 
 const MainLayout = ({ children }) => {
-  const bannerHeight = "60px";
+  // const bannerHeight = "60px";
 
   return (
     <div>
