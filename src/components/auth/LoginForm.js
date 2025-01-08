6dc22f134fcdf7e2ac75115973ddb3a726b60450
platypus3036 <akakehcn@gmail.com>
2025-01-09 02:07:09 +0900

    Temp

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import Button from '../common/Button';
 import { loginHandler } from '../../services/AuthService';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
